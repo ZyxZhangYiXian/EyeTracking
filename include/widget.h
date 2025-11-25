a69751796d9d3607b428645b22bd1b35c2ff7ee2
zyx <459279385@qq.com>
2025-11-25 20:29:13 +0800

    信号与槽设置

--- include/widget.h
+++ include/widget.h
@@ -2,6 +2,7 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QProcess>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -16,6 +17,9 @@ class Widget : public QWidget
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
+
+private slots:
+    void on_pushButton_clicked();
 
 private:
     Ui::Widget *ui;
--- src/widget.cpp
+++ src/widget.cpp
@@ -12,3 +12,13 @@ Widget::~Widget()
 {
     delete ui;
 }
+
+void Widget::on_pushButton_clicked()
+{
+    // 获取 lineedit 数据
+    QString program = ui->lineEdit->text();
+    // 创建一个 QProcess 对象
+    QProcess *my_process = new QProcess(this);
+    my_process->start(program);
+}
+
